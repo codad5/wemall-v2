f9dcaeae23cca8150990df5312025fb1a76e6cbf
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2023-06-14 17:06:09 +0100

    last working:

--- src/Controller/API/ProductController.php
+++ src/Controller/API/ProductController.php
@@ -20,43 +20,7 @@ use Codad5\PhpRouter\HTTP\Response;
 
 class ProductController
 {
-    static function upload_product(Request $req, Response $res): Response
-    {
-        $shopId = $req->params('id');
-        try{
-            $shop = Shop::find($shopId);
-            if(!$shop) throw new ShopException('Shop not found');
-            if (!Validator::validate_product_creation_data($shop->type, $req))
-                throw new CustomException("Error is shop Data", 300);
-            $product = Product::create($shop, $req->body());
-            return $res->redirect("/shop/$shopId/product?success=Product successfully created");
-        }catch(\Exception $e){
-            return $res->redirect("/shop/$shopId/product?error=".$e->getMessage());
-
-        }
-    }
-
-    static function edit_view(Request $req, Response $res): Response
-    {
-        try{
-            ['id' => $id, 'product_id' => $product_id] = $req->params();
-            $shop = Shop::find($id);
-            $product = $shop->findProduct($product_id)->toArray();
-            $shop = $shop->toArray();
-            $shop['form'] = function($product_type, $values = []){
-                return View\Shop::load_html_form($product_type, ['values' => $values]);
-            };
-            $product['form_action'] = 'edit';
-
-            // return $res->send(ViewLoader::load('html/edit_product.php', ["request" => $req, "shop" => $shop, "product" => $product]));
-            return $res->send(ViewLoader::load('html/ProductForms/main_form.php', ["shop" => $shop, "values" => $product]));
-        }catch(\Exception $e){
-            return $res->send(ViewLoader::load_error_page($e->getCode(), $e->getMessage()));
-            // return $res->redirect('/home?error='.$e->getMessage());
-
-        }
-    }
-    static function get_product(Request $req, Response $res)
+    static function get_product(Request $req, Response $res): Response
     {
         ['id' => $product_id] = $req->params();
         try{
@@ -68,7 +32,7 @@ class ProductController
             return ResponseHandler::sendErrorResponse($res, $exception->getMessage(), $exception->getCode());
         }
     }
-    static function get_all_product(Request $req, Response $res)
+    static function get_all_product(Request $req, Response $res): Response
     {
         try{
             $type_ = $type = $req->params("type");
