4082bc6d9dde6b976e22b85c2cdaebd693dd8049
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2023-07-09 02:19:06 +0100

    testing

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ use Codad5\Wemall\Libs\ErrorHandler;
 use Codad5\PhpRouter\Router as Router;
 use Codad5\PhpInex\Import as Import;
 
+
 $errorHandler = new ErrorHandler('index.php', true);
-echo "test";
+
 if (file_exists(__DIR__ . '/.env')) {
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 }
-echo "test";
 
 $envVariables = getenv();
 if (is_array($envVariables)) {
@@ -20,28 +20,21 @@ if (is_array($envVariables)) {
         $_ENV[$key] = $value;
     }
 }
-echo "test";
 
 
 
 $router = new Router(__DIR__ . "/src/view/", "/");
-echo "test";
 
 $router->allowed(['application/json', 'application/xml', 'text/html', 'text/plain', 'application/x-www-form-urlencoded', 'multipart/form-data']);
 
-echo "test";
 
 /** @var Router $app_routes */
 $app_routes = Import::this('src/Routes/APP.php');
-echo "test";
 /** @var Router $api_routes */
-//$api_routes = Import::this('src/Routes/API.php');
-//echo "test";
+$api_routes = Import::this('src/Routes/API.php');
 
 
-//$router->use_route($api_routes);
-//echo "test";
+$router->use_route($api_routes);
 $router->use_route($app_routes);
-echo "test";
 
 $router->serve();
